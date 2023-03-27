975ab457f28f94663c04fffc3c17ecf906ab5116
Francisco Orozco ActiveIT-ThinkPad <francisco.s.orozco@outlook.com>
2023-03-27 14:11:50 +0100

    first commit

---
+++ PluginGov2.ps1
@@ -0,0 +1,31 @@
+﻿# Silent Update autenticacao.gov 
+
+# Detect if the APP its installed on the system.
+$app = "plugin Autenticação.Gov*"
+Get-WmiObject -Class Win32_Product | Where-Object {$_.Name -like $app} | Select-Object Name
+
+
+if ($?) {
+  $workdir = "c:\installer\"
+  $Url = "https://aplicacoes.autenticacao.gov.pt/plugin/Instalador%20plugin%20Autenticacao.Gov.msi"
+  $Path = "C:\installer\pluginAutenticacao.gov_Win_x64_signed.msi"
+  
+  # Check if work directory exists if not create it 
+  If (Test-Path -Path $workdir -PathType Container)
+  { Write-Host "$workdir already exists" -ForegroundColor Red}
+  ELSE
+  { New-Item -Path $workdir -ItemType directory }
+  
+  # Download the .msi
+  $WebClient = New-Object System.Net.WebClient
+  $WebClient.DownloadFile( $url, $path )
+  
+  # Start the update 
+  Start-Process -FilePath msiexec.exe -ArgumentList "/i $Path /norestart" -Wait
+  
+  # Remove the installer 
+  Remove-Item -Force C:\installer\pluginAutenticacao.gov_Win_x64_signed.msi
+
+} else {
+  Write-Host "La aplicación $app NO está instalada."
+}
---
+++ comprobarapp.ps1
@@ -0,0 +1,9 @@
+﻿$nombreApp = "plugin Autenticação.Gov"
+
+$instalaciones = Get-Package | Where-Object {$_.Name -like "*$nombreApp*"}
+
+if ($instalaciones) {
+    Write-Output "La aplicación $nombreApp está instalada en este equipo."
+} else {
+    Write-Output "La aplicación $nombreApp no está instalada en este equipo."
+}
